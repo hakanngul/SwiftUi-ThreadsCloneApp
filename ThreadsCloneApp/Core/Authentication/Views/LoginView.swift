a829eff20b819577685d9069583768d6fcf308df
hhakangull <hakanngul@icloud.com>
2023-11-13 00:29:40 +0300

    Login and Signup screen completed

--- ThreadsCloneApp/Core/Authentication/Views/LoginView.swift
+++ ThreadsCloneApp/Core/Authentication/Views/LoginView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack {
+                Spacer()
+                Image("treads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120,height: 120)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .modifier(ThreadsTextFieldModifier())
+                    SecureField("Enter your password", text: $password)
+                        .modifier(ThreadsTextFieldModifier())
+                }
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing,28)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+                }
+                
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                Spacer()
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account? ")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
     }
 }
 
--- ThreadsCloneApp/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
+++ ThreadsCloneApp/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
@@ -5,4 +5,17 @@
 //  Created by Hakan Gül on 12/11/2023.
 //
 
-import Foundation
+
+import SwiftUI
+
+struct ThreadsTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+        
+    }
+}
