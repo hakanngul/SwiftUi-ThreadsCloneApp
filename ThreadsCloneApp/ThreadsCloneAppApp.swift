1cb31f633d101080f1654c7174c4375c71cbbaf1
hhakangull <hakanngul@icloud.com>
2023-11-12 22:35:30 +0300

    Initial Commit

---
+++ ThreadsCloneApp/ThreadsCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneAppApp.swift
+//  ThreadsCloneApp
+//
+//  Created by Hakan Gül on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
