1688a65538c034997e83b8377c2417e702670b3a
Hakan <hakanngul@icloud.com>
2023-11-22 00:53:54 +0300

    firebase configure completed

--- ThreadsCloneApp/App/ThreadsCloneAppApp.swift
+++ ThreadsCloneApp/App/ThreadsCloneAppApp.swift
@@ -6,13 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ThreadsCloneAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
 //            ContentView()
-            CreateThreadView()
+//            CreateThreadView()
+            ThreadsTabView()
         }
     }
 }
